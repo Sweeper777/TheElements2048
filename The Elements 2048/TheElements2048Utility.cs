04076e47e7b26b8cceaa83d7b5542ca5fef69bd7
Sweeper777 <sumulang@gmail.com>
2018-02-18 08:54:08 +0000

    refactored old code

--- The Elements 2048/TheElements2048Utility.cs
+++ The Elements 2048/TheElements2048Utility.cs
@@ -6,40 +6,46 @@ using System.Text;
 using System.Threading.Tasks;
 using The_Elements_2048.Properties;
 
-namespace The_Elements_2048 {
-    public static class TheElements2048Utility {
-        public static Dictionary<Element, Image> GetElementImageDictionary () {
-            Dictionary<Element, Image> dic = new Dictionary<Element, Image> ();
-            dic.Add (Element.Aluminium, Resources.Al);
-            dic.Add (Element.Berylium, Resources.Be);
-            dic.Add (Element.Boron, Resources.B);
-            dic.Add (Element.Carbon, Resources.C);
-            dic.Add (Element.Chlorine, Resources.Cl);
-            dic.Add (Element.Fluorine, Resources.F);
-            dic.Add (Element.Helium, Resources.He);
-            dic.Add (Element.Hydrogen, Resources.H);
-            dic.Add (Element.Lithium, Resources.Li);
-            dic.Add (Element.Magnesium, Resources.Mg);
-            dic.Add (Element.Neon, Resources.Ne);
-            dic.Add (Element.Nitrogen, Resources.N);
-            dic.Add (Element.Oxygen, Resources.O);
-            dic.Add (Element.Phosphorus, Resources.P);
-            dic.Add (Element.Silicon, Resources.Si);
-            dic.Add (Element.Sodium, Resources.Na);
-            dic.Add (Element.Sulphur, Resources.S);
-            return dic;
-        }
+namespace The_Elements_2048
+{
+    public static class TheElements2048Utility
+    {
+        public static IReadOnlyDictionary<Element, Image> ElementImageDictionary { get; }
 
-        public static Dictionary<Element, int> GetElementScoreDictionary () {
-            Dictionary<Element, int> returnVal = new Dictionary<Element,int> ();
+        public static IReadOnlyDictionary<Element, int> ElementScoreDictionary { get; }
+
+        static TheElements2048Utility()
+        {
+            var mutableElementImageDictionary = new Dictionary<Element, Image>();
+            mutableElementImageDictionary.Add(Element.Aluminium, Resources.Al);
+            mutableElementImageDictionary.Add(Element.Berylium, Resources.Be);
+            mutableElementImageDictionary.Add(Element.Boron, Resources.B);
+            mutableElementImageDictionary.Add(Element.Carbon, Resources.C);
+            mutableElementImageDictionary.Add(Element.Chlorine, Resources.Cl);
+            mutableElementImageDictionary.Add(Element.Fluorine, Resources.F);
+            mutableElementImageDictionary.Add(Element.Helium, Resources.He);
+            mutableElementImageDictionary.Add(Element.Hydrogen, Resources.H);
+            mutableElementImageDictionary.Add(Element.Lithium, Resources.Li);
+            mutableElementImageDictionary.Add(Element.Magnesium, Resources.Mg);
+            mutableElementImageDictionary.Add(Element.Neon, Resources.Ne);
+            mutableElementImageDictionary.Add(Element.Nitrogen, Resources.N);
+            mutableElementImageDictionary.Add(Element.Oxygen, Resources.O);
+            mutableElementImageDictionary.Add(Element.Phosphorus, Resources.P);
+            mutableElementImageDictionary.Add(Element.Silicon, Resources.Si);
+            mutableElementImageDictionary.Add(Element.Sodium, Resources.Na);
+            mutableElementImageDictionary.Add(Element.Sulphur, Resources.S);
+            ElementImageDictionary = mutableElementImageDictionary;
+
+            var mutableScoreDict = new Dictionary<Element, int>();
             Element keyIterator = Element.Hydrogen;
             int valueIterator = 2;
-            while (keyIterator < (Element)18) {
-                returnVal.Add (keyIterator, valueIterator);
+            while (keyIterator < (Element)18)
+            {
+                mutableScoreDict.Add(keyIterator, valueIterator);
                 valueIterator *= 2;
-                keyIterator = (Element)( ( (int)keyIterator ) + 1 );
+                keyIterator = (Element)(((int)keyIterator) + 1);
             }
-            return returnVal;
+            ElementScoreDictionary = mutableScoreDict;
         }
     }
 }
