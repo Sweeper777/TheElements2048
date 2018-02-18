48dbf83f1547d8c434b56eeab0f33927c774a120
Sweeper777 <sumulang@gmail.com>
2018-02-18 08:55:24 +0000

    added ToJagged<T> extension method

--- The Elements 2048/MatrixUtility.cs
+++ The Elements 2048/MatrixUtility.cs
@@ -7,5 +7,9 @@ namespace The_Elements_2048
         public static T[] Row<T>(this T[,] matrix, int row) {
             return Enumerable.Range(0, matrix.GetLength(0)).Select(x => matrix[x, row]).ToArray();
         }
+
+        public static T[][] ToJagged<T>(this T[,] matrix) {
+            return Enumerable.Range(0, matrix.GetLength(1)).Select(x => matrix.Row(x)).ToArray();
+        }
     }
 }
