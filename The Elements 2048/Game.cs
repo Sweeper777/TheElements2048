325be3fcd921729bf8d935a6cbfe22005af24a51
Sweeper777 <sumulang@gmail.com>
2018-02-20 07:04:49 +0000

    added Game properties

---
+++ The Elements 2048/Game.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Text;
+namespace The_Elements_2048
+{
+    public class Game
+    {
+        Random random = new Random();
+
+        const int boardSize = 4;
+
+        public int Score { get; set; } = 0;
+        public Element[,] Board { get; set; } = new Element[boardSize, boardSize];
+
+    }
+}
