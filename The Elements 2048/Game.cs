5007b1ca5b237da86878c4928441b333a2fcb6c7
Sweeper777 <sumulang@gmail.com>
2018-02-20 07:05:14 +0000

    added EmptySpaces property

--- The Elements 2048/Game.cs
+++ The Elements 2048/Game.cs
@@ -14,5 +14,19 @@ namespace The_Elements_2048
         public int Score { get; set; } = 0;
         public Element[,] Board { get; set; } = new Element[boardSize, boardSize];
 
+        public List<(int x, int y)> EmptySpaces { 
+            get {
+                var emptySpaces = new List<(int x, int y)>();
+                for (int i = 0; i < Board.GetLength(0); i++) {
+                    for (int j = 0; j < Board.GetLength(1); j++) {
+                        if (Board[i, j] == Element.None) {
+                            emptySpaces.Add((i, j));
+                        }
+                    }
+                }
+                return emptySpaces;
+            }
+        }
+
     }
 }
