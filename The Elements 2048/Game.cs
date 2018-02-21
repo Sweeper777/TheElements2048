45b7b56870f893bb0fd56e71b1310e9fbe4bb6dc
Sweeper777 <sumulang@gmail.com>
2018-02-21 07:43:54 +0000

    added MoveDown

--- The Elements 2048/Game.cs
+++ The Elements 2048/Game.cs
@@ -89,5 +89,12 @@ namespace The_Elements_2048
             Board = newBoard;
             return moved;
         }
+
+        public bool MoveDown() {
+            var newBoard = EvaluateBoard(Board.RotateClockwise()).RotateCounterClockwise(); 
+            var moved = !newBoard.Cast<Element>().SequenceEqual(Board.Cast<Element>());
+            Board = newBoard;
+            return moved;
+        }
     }
 }
