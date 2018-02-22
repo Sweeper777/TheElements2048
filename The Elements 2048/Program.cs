2b9161e5e12e2345fb8a8d4b677cdb0de6a48b01
Sweeper777 <sumulang@gmail.com>
2018-02-22 06:33:21 +0000

    added a console program to run 2048

--- The Elements 2048/Program.cs
+++ The Elements 2048/Program.cs
@@ -11,9 +11,40 @@ namespace The_Elements_2048 {
         /// </summary>
         [STAThread]
         static void Main () {
-            Application.EnableVisualStyles ();
-            Application.SetCompatibleTextRenderingDefault (false);
-            Application.Run (new GameWindow ());
+            //Application.EnableVisualStyles ();
+            //Application.SetCompatibleTextRenderingDefault (false);
+            //Application.Run (new GameWindow ());
+
+            var game = new Game();
+            game.SpawnNewElement();
+            game.SpawnNewElement();
+            Console.WriteLine(game);
+            while (true) {
+                bool moved;
+                var key = Console.ReadKey();
+                Console.WriteLine();
+                switch (key.Key) {
+                    case ConsoleKey.DownArrow:
+                        moved = game.MoveDown();
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        moved = game.MoveLeft();
+                        break;
+                    case ConsoleKey.RightArrow:
+                        moved = game.MoveRight();
+                        break;
+                    case ConsoleKey.UpArrow:
+                        moved = game.MoveUp();
+                        break;
+                    default:
+                        moved = false;
+                        break;
+                }
+                if (moved) {
+                    game.SpawnNewElement();
+                }
+                Console.WriteLine(game);
+            }
         }
     }
 }
