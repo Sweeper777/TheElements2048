d1ca7f305a6c9ab0ae19d7fa126a2e03fbf6163a
Sweeper777 <sumulang@gmail.com>
2018-02-25 08:18:58 +0000

    added Position struct

---
+++ The Elements 2048/Position.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace The_Elements_2048
+{
+    public struct Position
+    {
+        public int X { get; }
+        public int Y { get; }
+
+        public Position(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
