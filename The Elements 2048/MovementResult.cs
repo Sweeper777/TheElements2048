86cc0929bc4bf068b2a840f3ea77957e95b7b96e
Sweeper777 <sumulang@gmail.com>
2018-02-25 08:19:12 +0000

    added MovementResult

---
+++ The Elements 2048/MovementResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace The_Elements_2048
+{
+    public struct MovementResult
+    {
+        public bool Merged { get; }
+        public int DistanceMoved { get; }
+
+        public MovementResult(bool merged, int distanceMoved)
+        {
+            Merged = merged;
+            DistanceMoved = distanceMoved;
+        }
+    }
+}
