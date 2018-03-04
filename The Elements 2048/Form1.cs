c052f6c67af5aefa00af95fa5e0070711ddb3680
Sweeper777 <sumulang@gmail.com>
2018-03-04 09:07:59 +0000

    added GetPositionForCoordinate

--- The Elements 2048/Form1.cs
+++ The Elements 2048/Form1.cs
@@ -34,5 +34,13 @@ namespace The_Elements_2048
 
         private void GameWindow_KeyDown(object sender, KeyEventArgs e)
         {
+        Point GetPositionForCoordinate(int x, int y)
+        {
+            return new Point(
+                x * 100 + (x + 1) * 15,
+                y * 100 + (y + 1) * 15
+            );
+        }
+
         }
 }
