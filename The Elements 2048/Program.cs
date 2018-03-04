7f1f999408f79fea723eda5493664508b77f056c
Sweeper777 <sumulang@gmail.com>
2018-03-04 08:45:13 +0000

    opened GameWindow

--- The Elements 2048/Program.cs
+++ The Elements 2048/Program.cs
@@ -11,40 +11,40 @@ namespace The_Elements_2048 {
         /// </summary>
         [STAThread]
         static void Main () {
-            //Application.EnableVisualStyles ();
-            //Application.SetCompatibleTextRenderingDefault (false);
-            //Application.Run (new GameWindow ());
+            Application.EnableVisualStyles ();
+            Application.SetCompatibleTextRenderingDefault (false);
+            Application.Run (new GameWindow ());
 
-            var game = new Game();
-            game.SpawnNewElement();
-            game.SpawnNewElement();
-            Console.WriteLine(game);
-            while (true) {
-                bool moved;
-                var key = Console.ReadKey();
-                Console.WriteLine();
-                switch (key.Key) {
-                    case ConsoleKey.DownArrow:
-                        moved = game.MoveDown().HasMoved;
-                        break;
-                    case ConsoleKey.LeftArrow:
-                        moved = game.MoveLeft().HasMoved;
-                        break;
-                    case ConsoleKey.RightArrow:
-                        moved = game.MoveRight().HasMoved;
-                        break;
-                    case ConsoleKey.UpArrow:
-                        moved = game.MoveUp().HasMoved;
-                        break;
-                    default:
-                        moved = false;
-                        break;
-                }
-                if (moved) {
-                    game.SpawnNewElement();
-                }
-                Console.WriteLine(game);
-            }
+            //var game = new Game();
+            //game.SpawnNewElement();
+            //game.SpawnNewElement();
+            //Console.WriteLine(game);
+            //while (true) {
+            //    bool moved;
+            //    var key = Console.ReadKey();
+            //    Console.WriteLine();
+            //    switch (key.Key) {
+            //        case ConsoleKey.DownArrow:
+            //            moved = game.MoveDown().HasMoved;
+            //            break;
+            //        case ConsoleKey.LeftArrow:
+            //            moved = game.MoveLeft().HasMoved;
+            //            break;
+            //        case ConsoleKey.RightArrow:
+            //            moved = game.MoveRight().HasMoved;
+            //            break;
+            //        case ConsoleKey.UpArrow:
+            //            moved = game.MoveUp().HasMoved;
+            //            break;
+            //        default:
+            //            moved = false;
+            //            break;
+            //    }
+            //    if (moved) {
+            //        game.SpawnNewElement();
+            //    }
+            //    Console.WriteLine(game);
+            //}
         }
     }
 }
