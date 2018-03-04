47d59aee86ba2be5f100155ccee9da7dcb030c4d
Sweeper777 <sumulang@gmail.com>
2018-03-04 09:07:14 +0000

    something weird happened again

---
+++ The Elements 2048/Form1.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using ReactiveAnimation;
+using GameEssentials;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace The_Elements_2048
+{
+    public partial class GameWindow : Form
+    {
+        public GameWindow()
+        {
+            InitializeComponent();
+            //Game.CreateNewGame (this.panel1, Current_ScoreChanged, Current_HighscoreChanged, this.lblBestValue);
+        }
+
+        //void Current_HighscoreChanged (object sender, EventArgs e) {
+        //    this.lblBestValue.Text = Game.Current.Highscore.ToString ();
+        //}
+
+        //void Current_ScoreChanged (object sender, EventArgs e) {
+        //    this.lblScoreValue.Text = Game.Current.Score.ToString ();
+        //}
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            //    Game.Current.Dispose ();
+            //    Game.CreateNewGame (this.panel1, Current_ScoreChanged, Current_HighscoreChanged);
+            //    this.lblScoreValue.Text = "0";
+        }
+
+        private void GameWindow_KeyDown(object sender, KeyEventArgs e)
+        {
+        }
+}
